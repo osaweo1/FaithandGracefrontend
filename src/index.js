9f8618e83857465febe24ccf635acac66dd12b06
osamudiamen osawe <osamudiamenosawe@gmail.com>
2023-07-25 11:49:04 +0100

    update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+// import {disableReactDevTools} from '@fvilers-/disable-react-devtools'
+import {disableReactDevTools}from '@fvilers/disable-react-devtools'
 import { AppProvider } from './context';
 // import Layout from './layout/Layout';
+
+
+if(process.env.NODE_ENV==='production')disableReactDevTools()
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
